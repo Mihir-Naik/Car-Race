132a32415953c27d1eea174d109a14b59123f07f
Mihir Naik <mdnaik17@gmail.com>
2017-09-05 14:50:46 -0700

    left and right arrow keys now confined to container

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 $(function(){
 
 // Variables
-
+var $start = $('#start')
 var $container = $('#container')
 var $car = $('#car')
 var $line1 = $('#line1')
@@ -13,6 +13,7 @@ var $line6 = $('#line6')
 var $line7 = $('#line7')
 var $line8 = $('#line8')
 var $line9 = $('#line9')
+var gameOver = false
 var goLeft = false
 var goRight = false
 
@@ -44,16 +45,23 @@ $(document).on('keyup', function(e){
 
 // To move left
 function left() {
+    if (gameOver === false && parseInt($car.css('left'))>5) {
     $car.css('left', parseInt($car.css('left')) - 3);
     goLeft = requestAnimationFrame(left);
+    }
 }
 // To move right
 function right() {
+    if ((gameOver === false) && (parseInt($car.css('left')) < $container.width() - $car.width() - 5)){
     $car.css('left', parseInt($car.css('left')) + 3);
     goRight = requestAnimationFrame(right);
+    }
 }
 
-// animation = requestAnimationFrame(gameOn)
+$start.on('click', function() {
+
+
+animation = requestAnimationFrame(gameOn)
 function gameOn(){
     rollDown($line1)
     rollDown($line2)
@@ -66,6 +74,7 @@ function gameOn(){
     rollDown($line9)
     animation = requestAnimationFrame(gameOn)
 }
+})
 // Line keeps rolling down continously
 function rollDown(line) {
     var currentTop = parseInt(line.css('top'))
@@ -75,5 +84,13 @@ function rollDown(line) {
     line.css('top', currentTop + 5)
 }
 
+// function Box() {
+//     this.html = $('<div>').addClass('box')
+//     $container.append(this.html)
+
+// }
+
+// someBox = new Box()
+// console.log(someBox)
 // End
 });
