6f618c75c58c3748bcbc75538adc005b3a0c727b
Mihir Naik <mdnaik17@gmail.com>
2017-09-05 09:35:56 -0700

    left and right arrow keys work but car goes out of the container

--- js/main.js
+++ js/main.js
@@ -1,1 +1,36 @@
-console.log(jQuery)
+$(function(){
+var $container = $('#container')
+var $car = $('#car')
+var goLeft = false
+var goRight = false
+
+$(document).on('keydown', function(e){
+    var key = e.keyCode;
+    if (key === 37 && goLeft === false) {
+        goLeft = requestAnimationFrame(left);
+    } else if (key === 39 && goRight === false) {
+        goRight = requestAnimationFrame(right);
+    }
+})
+$(document).on('keyup', function(e){
+    var key = e.keyCode;
+    if (key === 37){
+        cancelAnimationFrame(goLeft);
+        goLeft = false;
+    } else if (key === 39){
+        cancelAnimationFrame(goRight);
+        goRight = false;
+    }
+})
+
+function left() {
+    $car.css('left', parseInt($car.css('left')) - 5);
+    goLeft = requestAnimationFrame(left);
+}
+
+function right() {
+    $car.css('left', parseInt($car.css('left')) + 5);
+    goRight = requestAnimationFrame(right);
+}
+
+});
